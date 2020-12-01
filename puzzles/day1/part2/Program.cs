c4f21837c7589fd13f850d99ada783b735f3bbd9
Kelson Ball <kelson.ball@hey.com>
2020-12-01 13:26:29 -0800

    cleanup

--- puzzles/day1/part2/Program.cs
+++ puzzles/day1/part2/Program.cs
@@ -1,23 +1,19 @@
 ﻿using static System.Console;
 using System.Collections.Generic;
 
-var numbers = new HashSet<int>();
+var numbers = new List<int>(2000);
 var solutions = new Dictionary<int, (int, int)>();
 
-string line = "";
-while ((line = ReadLine()?.Trim()) != "END")
+while (int.TryParse(ReadLine().Trim(), out int number))
 {
-    if (int.TryParse(line, out int number))
+    if (solutions.TryGetValue(number, out (int a, int b) pair))
     {
-        if (solutions.TryGetValue(number, out (int a, int b) pair))
-        {
-            WriteLine(number * pair.a * pair.b);
-            return;
-        }
-
-        foreach (var existing in numbers)
-            if (existing + number < 2020)
-                solutions.TryAdd(2020 - existing - number, (existing, number));
-        numbers.Add(number);
+        WriteLine(number * pair.a * pair.b);
+        return;
     }
+
+    foreach (var existing in numbers)
+        if (existing + number < 2020)
+            solutions.TryAdd(2020 - existing - number, (existing, number));
+    numbers.Add(number);
 }
