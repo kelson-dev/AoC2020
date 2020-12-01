d9f023a12b5f2cce65db40906dc8b8cd5b804f3a
Kelson Ball <kelson.ball@hey.com>
2020-12-01 13:58:37 -0800

    d1p2 rust

--- puzzles/day1/part2rs/src/main.rs
+++ puzzles/day1/part2rs/src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::BTreeSet;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers: BTreeSet<u32> = BTreeSet::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let number = line.unwrap().parse::<u32>().unwrap();
+        for previous in &numbers {
+            if number + previous < 2020 {
+                let solution = 2020 - number - previous;
+                if numbers.contains(&solution) {
+                    println!("{}", solution * number * previous);
+                    return;
+                }
+            }
+        }
+        numbers.insert(number);
+    }
 }
