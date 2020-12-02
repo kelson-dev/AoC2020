0a85b77de9d945cc19b0401b59a0af87515958be
Kelson Ball <kelson.ball@hey.com>
2020-12-01 23:35:12 -0800

    Eliminate heap allocs from d2cs

--- puzzles/day2/part1cs/Program.cs
+++ puzzles/day2/part1cs/Program.cs
@@ -1,37 +1,54 @@
 ﻿using static System.Console;
 using System;
-using static System.IO.File;
-
-var lines = ReadAllLines("puzzle.input");
+using System.IO;
 
 var valid = 0;
-foreach (var line in lines)
+using var stream = File.OpenRead("puzzle.input");
+using var reader = new StreamReader(stream);
+unsafe
 {
-    var span = line.AsSpan();
-    valid += new Policy(ref span).Evaluate(span) ? 1 : 0;
+    Span<char> buffer = stackalloc char[32];
+
+    int read = -1;
+    int offset = 0;
+    var range = buffer[offset..];
+    while ((read = reader.Read(range)) > 0)
+    {
+        var line = buffer[..buffer.IndexOf('\n')];
+        var length = line.Length + 1;
+        valid += new Policy(ref line).Evaluate(line) ? 1 : 0;
+        var remaining = buffer[length..];
+        remaining.CopyTo(buffer);
+        range = buffer[remaining.Length..];
+    }
 }
 
 WriteLine(valid);
 
 public readonly ref struct Policy
 {
-    public readonly int First;
-    public readonly int Second;
+    public readonly int Min;
+    public readonly int Max;
     public readonly char Letter;
 
-    public Policy(ref ReadOnlySpan<char> source)
+    public Policy(ref Span<char> source)
     {
         var (dash, space) = (source.IndexOf('-'), source.IndexOf(' '));
         var first = source[..dash++];
         var second = source[dash..space++];
 
-        First = int.Parse(first);
-        Second = int.Parse(second);
+        Min = int.Parse(first);
+        Max = int.Parse(second);
         Letter = source[space];
         source = source[(first.Length + second.Length + 5)..];
     }
 
-    public bool Evaluate(ReadOnlySpan<char> pass) => 
-           (pass[First - 1] == Letter && pass[Second - 1] != Letter)
-        || (pass[First - 1] != Letter && pass[Second - 1] == Letter);
+    public bool Evaluate(Span<char> pass)
+    {
+        int count = 0;
+        foreach (var character in pass)
+            if (character == Letter)
+                count++;
+        return count >= Min && count <= Max;
+    }
 }
