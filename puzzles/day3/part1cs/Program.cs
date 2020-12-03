9f26bfed54338d8a765804a4315cc592ddde094c
Kelson Ball <kelson.ball@hey.com>
2020-12-03 11:30:18 -0800

    d3 cs

---
+++ puzzles/day3/part1cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Linq;
+
+const char tree = '#';
+
+int width = 31;
+
+var map = File.ReadAllLines("puzzle.input");
+
+var column = 0;
+var dc = 3;
+int trees = 0;
+
+for (int row = 0; row < map.Length; row++)
+{
+    var line = map[row];
+    if (line[column % width] == tree)
+        trees++;
+    column = column + dc;
+}
+
+
+
+WriteLine(trees);
---
+++ puzzles/day3/part2cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Linq;
+
+const char tree = '#';
+
+(int, int)[] dirs = new []
+{ 
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+};
+
+int width = 31;
+
+var map = File.ReadAllLines("puzzle.input");
+
+var treesOnEach = dirs
+    .Select(TreesOnRoute)
+    .Aggregate(1L, (a, t) => a * t);
+
+WriteLine(treesOnEach);
+
+long TreesOnRoute((int, int) direction)
+{
+    var (dc, dr) = direction;
+    var (column, row) = (0, 0);
+    long trees = 0;
+
+    while (row < map.Length)
+    {
+        var line = map[row];
+        if (line[column % width] == tree)
+            trees++;
+        (column, row) = (column + dc, row + dr);
+    }
+    return trees;
+}
