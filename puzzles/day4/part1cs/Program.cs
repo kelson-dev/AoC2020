34f733a4333283782d0529b8d9f2b123ee93d431
Kelson Ball <kelson.ball@hey.com>
2020-12-04 13:57:33 -0800

    d4p2cs

--- puzzles/day4/part1cs/Program.cs
+++ puzzles/day4/part1cs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using static System.IO.File;
 using static System.Console;
 using System.Linq;
+using System.Collections.Generic;
 
 var required = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
 
@@ -10,10 +11,12 @@ WriteLine(
         .Replace("♥♥", "\n")
         .Replace("♥", " ")
         .Split("\n")
-        .Select(passport => 
+        .Select(passport =>
             passport.Trim()
                 .Split(' ')
                 .Select(kvp => kvp.Split(':'))
                 .ToDictionary(kvp => kvp[0], kvp => kvp[1]))
-        .Where(passport => required.All(key => passport.ContainsKey(key)))
+        .Where(IsValidPassport)
         .Count());
+
+bool IsValidPassport(Dictionary<string, string> passport) => required.All(key => passport.ContainsKey(key));
---
+++ puzzles/day4/part2cs/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.IO.File;
+using static System.Console;
+using System.Linq;
+using System.Collections.Generic;
+
+var required = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+var validEyeColors = new HashSet<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+
+WriteLine(
+    ReadAllText("puzzle.input")
+        .Replace("\n", "♥")
+        .Replace("♥♥", "\n")
+        .Replace("♥", " ")
+        .Split("\n")
+        .Select(passport =>
+            passport.Trim()
+                .Split(' ')
+                .Select(kvp => kvp.Split(':'))
+                .ToDictionary(kvp => kvp[0], kvp => kvp[1]))
+        .Where(IsValidPassport)
+        .Count());
+
+bool IsValidPassport(Dictionary<string, string> passport) => 
+       required.All(key => passport.ContainsKey(key))
+    && int.TryParse(passport["byr"], out int byr) && (byr >= 1920 && byr <= 2002)
+    && int.TryParse(passport["iyr"], out int iyr) && (iyr >= 2010 && iyr <= 2020)
+    && int.TryParse(passport["eyr"], out int eyr) && (eyr >= 2020 && eyr <= 2030)
+    && IsValidHeight(passport["hgt"])
+    && IsValidColor(passport["hcl"])
+    && validEyeColors.Contains(passport["ecl"])
+    && passport["pid"].Length == 9 && passport["pid"].All(char.IsDigit);
+
+bool IsValidHeight(string hgt)
+{
+    if (hgt.EndsWith("cm"))
+        return int.TryParse(hgt[..^2], out int hgtCm) && hgtCm >= 150 && hgtCm <= 193;
+    else if (hgt.EndsWith("in"))
+        return int.TryParse(hgt[..^2], out int hgtIn) && hgtIn >= 59 && hgtIn <= 76;
+    else
+        return false;
+}
+
+bool IsValidColor(string color) =>
+       color.Length == 7
+    && color.StartsWith("#")
+    && color[1..].All(c => char.IsDigit(c) || (c >= 'a' && c <= 'f'));
