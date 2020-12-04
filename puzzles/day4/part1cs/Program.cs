89dc5004ee1a518836d683e80f966ff219b751f3
Kelson Ball <kelson.ball@hey.com>
2020-12-04 13:21:16 -0800

    d4p1cs

---
+++ puzzles/day4/part1cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.IO.File;
+using static System.Console;
+using System.Linq;
+
+var required = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+
+WriteLine(
+    ReadAllText("puzzle.input")
+        .Replace("\n", "♥")
+        .Replace("♥♥", "\n")
+        .Replace("♥", " ")
+        .Split("\n")
+        .Select(passport => 
+            passport.Trim()
+                .Split(' ')
+                .Select(kvp => kvp.Split(':'))
+                .ToDictionary(kvp => kvp[0], kvp => kvp[1]))
+        .Where(passport => required.All(key => passport.ContainsKey(key)))
+        .Count());
