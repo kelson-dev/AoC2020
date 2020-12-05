83b7a58d166843dbeaf4195a2f1dbf32283986f5
Kelson Ball <kelson.ball@hey.com>
2020-12-04 21:52:26 -0800

    "Isn't that how numbers work?"

--- puzzles/day5/cs/Program.cs
+++ puzzles/day5/cs/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using static System.IO.File;
 using static System.Console;
 
@@ -16,24 +17,7 @@ for (int i = 1; i < seatsFound.Length; i++)
     lastFound = seatsFound[i];
 }
 
-int Id(string path)
-{
-    var (row, _) = Traverse(0, 127, path[..^3]);
-    var (column, _) = Traverse(0, 7, path[^3..]);
-    return (row * 8 + column);
-}
+int Id(string path) => Traverse(path[..^3]) * 8 + Traverse(path[^3..]);
 
-(int, int) Traverse(int rmin, int rmax, string path)
-{
-    path = path.Replace("R", "B").Replace("L", "F");
-    foreach (var c in path)
-    {
-        var dif = (rmax - rmin) / 2;
-        if (c == 'F')
-            rmax = rmax - dif - 1;
-        else if (c == 'B')
-            rmin = rmin + dif + 1;
-    }
-    return (rmin, rmax);
-}
+int Traverse(string path) => Convert.ToInt32(path.Replace("R", "1").Replace("B", "1").Replace("L", "0").Replace("F", "0"), 2);
 
