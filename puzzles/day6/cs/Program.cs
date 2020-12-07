1693728b90547d1e9d25560ef85a3d48ed92db2e
Kelson Ball <kelson.ball@hey.com>
2020-12-07 13:54:38 -0800

    add day6

---
+++ puzzles/day6/cs/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using static System.IO.File;
+using static System.Console;
+
+var input = ReadAllText("puzzle.input");
+
+WriteLine(input.Split("\n\n").Select(l => l.Replace("\n", "").ToHashSet()).Select(s => s.Count).Sum());
+
+WriteLine(input.Split("\n\n").Select(l => l.Split('\n').Aggregate(l.AsEnumerable(), (a, b) => a.Intersect(b))).Select(s => s.Count()).Sum());
